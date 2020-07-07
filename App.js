4df08b3c91b181cdf2f39c62a959ccc6d50eb3c2
rafael-barrantes <62612559+rafael-barrantes@users.noreply.github.com>
2020-07-07 12:22:35 -0600

    Enable BER-TLV parse

--- App.js
+++ App.js
@@ -11,6 +11,14 @@ import {
 } from 'react-native';
 import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 
+let TLV = require('node-tlv');
+
+const CARD_EXPIRATION_TAG = '5F24';
+const CARD_PAN_TAG = '5A';
+
+import { Buffer } from 'buffer';
+global.Buffer = Buffer
+
 class AppV2Apdu extends React.Component {
   componentDidMount() {
     NfcManager.start();
@@ -25,9 +33,22 @@ class AppV2Apdu extends React.Component {
       <View style={styles.centeredView}>
         <Text style={styles.textStyle}>NFC DEMO</Text>
         <Text style={styles.textStyle}></Text>
+
         <TouchableHighlight
           style={styles.openButton}
-          onPress={this._test}>
+          onPress={() => this.getCardValues('MASTERCARD')}>
+          <Text style={styles.buttonTextStyle}>MASTERCARD</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={styles.openButton}
+          onPress={() => this.getCardValues('VISA')}>
+          <Text style={styles.buttonTextStyle}>VISA</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={styles.openButton}
+          onPress={() => this.getCardValues('TEST')}>
           <Text style={styles.buttonTextStyle}>TEST</Text>
         </TouchableHighlight>
 
@@ -45,7 +66,7 @@ class AppV2Apdu extends React.Component {
     NfcManager.cancelTechnologyRequest().catch(() => 0);
   }
 
-  _test = async () => {
+  async getCardValues(aidOption) {
     try {
       Alert.alert('Reading..\nPlease bring the card closer!');
       let tech = NfcTech.IsoDep;
@@ -53,30 +74,13 @@ class AppV2Apdu extends React.Component {
         alertMessage: 'Ready to send some APDU'
       });
 
-      console.log();
-      console.log('<=====TECH=====>');
-      console.log(resp);
-      console.log();
-
-      // the NFC uid can be found in tag.id
-      let tag = await NfcManager.getTag();
-      //var stringTag = JSON.stringify(tag);
-      console.log('<=====TAG=====>');
-      console.log('<=====techTypes=====>');
-      console.log(tag.techTypes);
-
-      console.log('<=====id=====>');
-      console.log(tag.id);
-      console.log();
-
       Alert.alert('I got your tag');
-      console.log('<=====Platform.OS=====>');
-      console.log(Platform.OS);
+
       console.log();
 
       if (Platform.OS === 'android') {
 
-        //AID= Visa Proximity Payment System Environment – PPSE (2PAY.SYS.DDF01)
+        //Proximity Payment System Environment – PPSE (2PAY.SYS.DDF01)
         //0x32, 0x50, 0x41, 0x59, 0x2e, 0x53, 0x59, 0x53, 0x2e, 0x44, 0x44, 0x46, 0x30, 0x31, 0x00
         /*resp = await NfcManager.transceive([
           0x00, //CLA Class 
@@ -90,13 +94,6 @@ class AppV2Apdu extends React.Component {
         //VISA AID = A0000000031010 -> 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10,
         //MASTERCARD AID = A0000000041010 -> 0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10,
 
-        //let aidOption = "VISA";
-        //let aidOption = "MASTERCARD";
-
-        let aidOption = "MASTERCARD";
-
-        //00 b2 02 0c 00
-
         if (aidOption == "VISA") {
           //Application Selection
           resp = await NfcManager.transceive([
@@ -109,14 +106,16 @@ class AppV2Apdu extends React.Component {
           ]);
           //Initiate Application Process
           resp = await NfcManager.transceive([
-            0x80,
-            0xA8,
-            0x00,
-            0x00,
-            0x0A,
-            0x83, 0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00
+            0x80, //CLA Class
+            0xA8, //INS Instruction
+            0x00, //P1  Parameter 1
+            0x00, //P2  Parameter 2
+            0x02, //AID Length 
+            0x83, 0x00, //AID
+            0x00 //fix
           ]);
+          console.log('=====VISA=====')
+          console.log(this.decToHex(resp).join(' '));
         }
         if (aidOption == "MASTERCARD") {
           //Application Selection
@@ -146,27 +145,29 @@ class AppV2Apdu extends React.Component {
             0x14, //P2  Parameter 2
             0x00, //AID Length 
           ]);
+          console.log('=====MASTERCARD=====');
+
+          resp = this.decToHex(resp);
+          let tlv = TLV.parse(resp.join(""));
+          let tlv_child = tlv.child;
+
+          let expiration = this.getTagValue(tlv_child, CARD_EXPIRATION_TAG);
+
+          let pan = this.getTagValue(tlv_child, CARD_PAN_TAG);
+
+          console.log('=====expiration=====');
+          console.log(expiration);
+
+          console.log('=====pan=====');
+          console.log(pan);
+
         }
+
         if (aidOption == "TEST") {
-          resp = await NfcManager.transceive([
-            0x00, //CLA Class 
-            0xB2, //INS Instruction
-            0x01, //P1  Parameter 1
-            0x0C, //P2  Parameter 2
-            0x00, //AID Length 
-          ]);
+          resp = await this.getAID(); //Application Identifier (AID)
+          console.log(resp.join(' '))
+          this.getTagValue(resp);
         }
-
-        //Decimal response to Hexadecimal BER-TLV
-        resp.forEach((item, index, arr) => {
-          let hexString = item.toString(16);
-          let hexFixed = hexString.length == 1 ? 0 + hexString : hexString;
-          arr[index] = hexFixed;
-        });
-
-        let ber_tlv = resp.join(" ");
-        console.log('<=====BER-TLV=====>');
-        console.log(ber_tlv)
 
       }
 
@@ -176,6 +177,28 @@ class AppV2Apdu extends React.Component {
       this._cleanUp();
     }
   }
+
+  getTagValue(tlv_child, tag) {
+    let value = null;
+    tlv_child.forEach((item) => {
+      if (item.tag == tag) {
+        value = item.value;
+      }
+    });
+
+    return value;
+  }
+
+  //parse decimal resp into hexadecimal BER-TVL
+  decToHex(resp) {
+    resp.forEach((item, index, arr) => {
+      let hexString = item.toString(16);
+      let hexFixed = hexString.length == 1 ? 0 + hexString : hexString;
+      arr[index] = hexFixed;
+    });
+    return resp;
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -199,7 +222,7 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
-    fontSize: 34,
+    fontSize: 24,
   },
   textStyle: {
     color: '#0000A0',
