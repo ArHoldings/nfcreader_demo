7197a2b416b6279d62cd10526bd3eec2f6dcecd8
rafael-barrantes <62612559+rafael-barrantes@users.noreply.github.com>
2020-07-08 16:45:02 -0600

    Reading data from Visa cards

--- App.js
+++ App.js
@@ -95,6 +95,7 @@ class AppV2Apdu extends React.Component {
         //MASTERCARD AID = A0000000041010 -> 0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10,
 
         if (aidOption == "VISA") {
+          console.log('=====VISA=====');
           //Application Selection
           resp = await NfcManager.transceive([
             0x00, //CLA Class 
@@ -104,20 +105,48 @@ class AppV2Apdu extends React.Component {
             0x07, //AID Length 
             0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10 //AID
           ]);
+          //console.log('=====Application Selection=====')
+          //console.log(this.decToHex(resp).join(' '));
+
           //Initiate Application Process
           resp = await NfcManager.transceive([
             0x80, //CLA Class
             0xA8, //INS Instruction
-            0x00, //P1  Parameter 1
+            0x00, //P1  Parameter 1 
             0x00, //P2  Parameter 2
-            0x02, //AID Length 
-            0x83, 0x00, //AID
-            0x00 //fix
+            0x23, //AID Length 
+            0x83, //Tag for PDOL
+            0x21,//Length of all data
+            //PDOL-data: 
+            0xF3, 0x20, 0x40, 0x00, //Tag: Terminal Transaction Qualifiers *required
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //Tag: Amount, Authorised (Numeric) 
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //Tag:  Amount, Other (Numeric)
+            0x09, 0x78, //Tag: Terminal Country Code *required
+            0x00, 0x00, 0x00, 0x00, 0x00, //Tag: Terminal Verification Results 
+            0x00, 0x00, //Tag: Transaction Currency Code
+            0x00, 0x00, 0x00, //Tag: Transaction Date
+            0x00, //Tag: Transaction Type
+            0x00, 0x00, 0x00, 0x00, //Tag:  Unpredictable Number 
+            0x00 //Fix
           ]);
-          console.log('=====VISA=====')
-          console.log(this.decToHex(resp).join(' '));
+          //console.log('=====Initiate Application Process=====')
+          //console.log(this.decToHex(resp).join(' '));
+
+          //Read Application Data 
+          resp = await NfcManager.transceive([
+            0x00, //CLA Class 
+            0xB2, //INS Instruction
+            0x01, //P1  start - first record
+            0x1C, //P2  Reference Control according Table
+            0x00, //LE  Number of bytes to be read
+          ]);
+
+          //console.log('=====Read Application Data=====')
+          //console.log(this.decToHex(resp).join(' '));
+
         }
         if (aidOption == "MASTERCARD") {
+          console.log('=====MASTERCARD=====');
           //Application Selection
           resp = await NfcManager.transceive([
             0x00, //CLA Class 
@@ -134,33 +163,18 @@ class AppV2Apdu extends React.Component {
             0x00, //P1  Parameter 1
             0x00, //P2  Parameter 2
             0x02, //AID Length 
-            0x83, 0x00, //AID
-            0x00 //fix
+            0x83, //Tag for PDOL
+            0x00, //Length of all data (Empty)
+            0x00 //Fix
           ]);
           //Read Application Data 
           resp = await NfcManager.transceive([
             0x00, //CLA Class 
             0xB2, //INS Instruction
-            0x01, //P1  Parameter 1
-            0x14, //P2  Parameter 2
-            0x00, //AID Length 
+            0x01, //P1  start - first record
+            0x14, //P2  Reference Control according Table
+            0x00, //LE  Number of bytes to be read
           ]);
-          console.log('=====MASTERCARD=====');
-
-          resp = this.decToHex(resp);
-          let tlv = TLV.parse(resp.join(""));
-          let tlv_child = tlv.child;
-
-          let expiration = this.getTagValue(tlv_child, CARD_EXPIRATION_TAG);
-
-          let pan = this.getTagValue(tlv_child, CARD_PAN_TAG);
-
-          console.log('=====expiration=====');
-          console.log(expiration);
-
-          console.log('=====pan=====');
-          console.log(pan);
-
         }
 
         if (aidOption == "TEST") {
@@ -168,6 +182,22 @@ class AppV2Apdu extends React.Component {
           console.log('TEST')
 
         }
+
+        resp = this.decToHex(resp);
+        let tlv = TLV.parse(resp.join(""));
+        let tlv_child = tlv.child;
+
+        let expiration = this.getTagValue(tlv_child, CARD_EXPIRATION_TAG);
+
+        let pan = this.getTagValue(tlv_child, CARD_PAN_TAG);
+
+        console.log('=====expiration=====');
+        Alert.alert('=====expiration=====', expiration);
+        console.log(expiration);
+
+        console.log('=====pan=====');
+        Alert.alert('=====pan=====', pan);
+        console.log(pan);
 
       }
 
